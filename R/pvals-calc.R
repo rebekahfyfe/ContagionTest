c916d180106ee41411c76e56c81334feb64295d0
rjf9 <rjf5775@psu.edu>
2023-01-30 13:02:56 -0500

    Change unit root test used

--- R/pvals-calc.R
+++ R/pvals-calc.R
@@ -5,13 +5,14 @@
 #' @param iterations An integer denoting number of iterations
 #' @param cores An integer denoting number of cores to run the parallelization on
 #'
-#' @return A numeric vector of p-values
+#' @return A numeric vector of p-values, length is the same as the number of iterations
 #' @export
 #'
 #' @examples
+#'
 PValsParallel <- function(df, iterations, cores = 1){
   myCluster <- parallel::makeCluster(cores, # number of cores to use
-                                    type = "PSOCK") # type of cluster
+                                     type = "PSOCK") # type of cluster
   doParallel::registerDoParallel(myCluster)
   results <- foreach(n = 1:iterations, .combine = "rbind", .packages = "punitroots") %dopar% {
     binindex <- sample(c(1, 2),size = length(df[, 1]), replace = T)
@@ -19,8 +20,8 @@ PValsParallel <- function(df, iterations, cores = 1){
     bin2 <- df[, binindex == 2]
     bindat <- cbind(apply(bin1, 1, mean), apply(bin2, 1, mean))
     bindat <- diff(bindat,1)
-    t <- punitroots::pCADFtest(bindat, crosscorr = 0.1)
-    pvals <-  t$p.value
+    t <- plm::purtest(bindat)
+    pvals <-  t$statistic$p.value
     return(pvals)
   }
   parallel::stopCluster(myCluster)
