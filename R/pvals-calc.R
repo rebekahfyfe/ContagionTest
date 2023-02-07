9f70031dd158fe81d6134cdd57c2cb8615455a26
rjf9 <rjf5775@psu.edu>
2023-02-07 11:31:32 -0500

    Add differencing option

--- R/pvals-calc.R
+++ R/pvals-calc.R
@@ -4,6 +4,7 @@
 #' @param df A dataframe of time series values. Should be formatted such that each column contains values for a unique year. Each row is a different node.
 #' @param iterations An integer denoting number of iterations to be run. Default is 100
 #' @param cores An integer denoting number of cores to run the parallelization on. Default is 1.
+#' @param stationary A binary indicator for whether df contains stationary data. Default is FALSE.
 #'
 #' @return A numeric vector of p-values, length is the same as the number of iterations
 #' @export
@@ -15,19 +16,33 @@
 #' #200 iterations run on 2 cores
 #' PValsParallel(df = polity, iterations = 200, cores = 2)
 #'
-PValsParallel <- function(df, iterations, cores = 1){
+PValsParallel <- function(df, iterations, cores = 1, stationary = FALSE){
+  require(foreach)
   myCluster <- parallel::makeCluster(cores, # number of cores to use
                                      type = "PSOCK") # type of cluster
   doParallel::registerDoParallel(myCluster)
   results <- foreach(n = 1:iterations, .combine = "rbind", .packages = "plm") %dopar% {
-    binindex <- sample(c(1, 2),size = length(df[, 1]), replace = T)
-    bin1 <- df[, binindex == 1]
-    bin2 <- df[, binindex == 2]
-    bindat <- cbind(apply(bin1, 1, mean), apply(bin2, 1, mean))
-    bindat <- diff(bindat,1)
-    t <- plm::purtest(bindat)
-    pvals <-  t$statistic$p.value
-    return(pvals)
+    x = try(plm::purtest(df), silent = T)
+    if(class(x) == "try-error" | stationary == FALSE){
+      ## do this if non stationary
+      binindex <- sample(c(1, 2), size = ncol(df), replace = T)
+      bin1 <- df[, binindex == 1]
+      bin2 <- df[, binindex == 2]
+      bindat <- cbind(apply(bin1, 1, mean), apply(bin2, 1, mean))
+      bindat <- diff(bindat, 1)
+      t <- plm::purtest(bindat)
+      pvals <-  t$statistic$p.value
+      return(pvals)
+    } else {
+      ## do this if stationary
+      binindex <- sample(c(1, 2), size = ncol(df), replace = T)
+      bin1 <- df[, binindex == 1]
+      bin2 <- df[, binindex == 2]
+      bindat <- cbind(apply(bin1, 1, mean), apply(bin2, 1, mean))
+      t <- plm::purtest(bindat)
+      pvals <-  t$statistic$p.value
+      return(pvals)
+    }
   }
   parallel::stopCluster(myCluster)
   results2 <- sapply(results, "[[", 1)
