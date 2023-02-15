11f5be5bce54a68d4f04355bf29d9662d1900457
rjf9 <rjf5775@psu.edu>
2023-02-15 10:28:41 -0500

    Add p-value threshold arg

--- R/parallel-contagion-test.R
+++ R/parallel-contagion-test.R
@@ -4,7 +4,8 @@
 #' @param df A dataframe with a column of nodes, a column of years, and a column of values
 #' @param iterations An integer denoting how many iterations of the test should be run
 #' @param cores An integer denoting how many cores to use, default is 1
-#' @param stationary A binary indicator for whether df contains stationary data. Default is FALSE.
+#' @param stationary A binary indicator for whether df should be differenced. Default is TRUE.
+#' @param threshold A threshold value for conducting a unit root test. If conducting a unit root test on df returns a p-value less than this value, the data will be differenced. Default is o.1.
 #'
 #' @return A dataframe with the coefficients from the contagion test
 #' @export
@@ -13,31 +14,53 @@
 #' #running parallelized contagion tests on the data
 #' ParallelContagionTest(df = polity90, iterations = 10000, cores = 3, stationary = FALSE)
 #'
-ParallelContagionTest <- function(df, iterations, cores = 1, stationary = FALSE){
+ParallelContagionTest <- function(df, iterations, cores = 1, difference = TRUE, threshold = 0.1){
+  require(foreach)
   myCluster <- parallel::makeCluster(cores, # number of cores to use
                                      type = "PSOCK") # type of cluster
   doParallel::registerDoParallel(myCluster)
   results <- foreach(n = 1:iterations, .combine = "rbind") %dopar% {
-    x = try(plm::purtest(df), silent = T)
-    if(class(x) == "try-error" | stationary == FALSE){
-      #for nonstationary data
-      j <- sample(c(1, 2), size = ncol(df), replace = TRUE) #produces unequal bins
-      Yj1 <- df[, j == 1]
-      Yj2 <- df[, j == 2]
+    if(difference == TRUE){
+      node_sd <- apply(df, 2, sd)
+      x <- plm::purtest(df[, node_sd > 0], test = "hadri", exo = "intercept")
+      if(x$statistic$p.value < threshold){
+        #for nonstationary data
+        j <- sample(c(1, 2), size = ncol(df), replace = TRUE) #produces unequal bins
+        Yj1 <- df[, j == 1]
+        Yj2 <- df[, j == 2]
 
-      j1mean <- apply(Yj1, 1, mean)
-      j2mean <- apply(Yj2, 1, mean)
-      j1mean <- diff(j1mean, 1)   # taking the difference between Y1 and Y0 for stationarity
-      j2mean <- diff(j2mean, 2)   # taking the difference between Y1 and Y0 for stationarity
-      j1mean.t <- j1mean[2:(length(j1mean))]
-      j2mean.t <- j2mean[2:(length(j2mean))]
+        j1mean <- apply(Yj1, 1, mean)
+        j2mean <- apply(Yj2, 1, mean)
+        j1mean <- diff(j1mean, 1)   # taking the difference between Y1 and Y0 for stationarity
+        j2mean <- diff(j2mean, 2)   # taking the difference between Y1 and Y0 for stationarity
+        j1mean.t <- j1mean[2:(length(j1mean))]
+        j2mean.t <- j2mean[2:(length(j2mean))]
 
-      j1mean.tm1 = j1mean[1:(length(j1mean)-1)]
-      j2mean.tm1 = j2mean[1:(length(j2mean)-1)]
+        j1mean.tm1 = j1mean[1:(length(j1mean)-1)]
+        j2mean.tm1 = j2mean[1:(length(j2mean)-1)]
 
-      regmod <- lm(c(j1mean.t, j2mean.t) ~ c(j1mean.tm1, j2mean.tm1) +
+        regmod <- lm(c(j1mean.t, j2mean.t) ~ c(j1mean.tm1, j2mean.tm1) +
                      c(j2mean.tm1, j1mean.tm1))
-      return(coefficients(regmod))
+        return(coefficients(regmod))
+      } else{
+          #for stationary data
+          j <- sample(c(1, 2), size = ncol(df), replace = TRUE) #produces unequal bins
+          Yj1 <- df[, j == 1]
+          Yj2 <- df[, j == 2]
+
+          j1mean <- apply(Yj1, 1, mean)
+          j2mean <- apply(Yj2, 1, mean)
+
+          j1mean.t <- j1mean[2:(length(j1mean))]
+          j2mean.t <- j2mean[2:(length(j2mean))]
+
+          j1mean.tm1 = j1mean[1:(length(j1mean)-1)]
+          j2mean.tm1 = j2mean[1:(length(j2mean)-1)]
+
+          regmod <- lm(c(j1mean.t, j2mean.t) ~ c(j1mean.tm1, j2mean.tm1) +
+                       c(j2mean.tm1, j1mean.tm1))
+          return(coefficients(regmod))
+      }
     } else{
       #for stationary data
       j <- sample(c(1, 2), size = ncol(df), replace = TRUE) #produces unequal bins
@@ -57,7 +80,6 @@ ParallelContagionTest <- function(df, iterations, cores = 1, stationary = FALSE)
                      c(j2mean.tm1, j1mean.tm1))
       return(coefficients(regmod))
     }
-
   }
   parallel::stopCluster(myCluster)
   return(results)
--- R/pvals-calc.R
+++ R/pvals-calc.R
@@ -4,7 +4,9 @@
 #' @param df A dataframe of time series values. Should be formatted such that each column contains values for a unique year. Each row is a different node.
 #' @param iterations An integer denoting number of iterations to be run. Default is 100
 #' @param cores An integer denoting number of cores to run the parallelization on. Default is 1.
-#' @param stationary A binary indicator for whether df contains stationary data. Default is FALSE.
+#' @param difference A binary indicator for whether df should be differenced. Default is TRUE.
+#' @param threshold A threshold value for conducting a unit root test. If conducting a unit root test on df returns a p-value less than this value, the data will be differenced. Default is o.1.
+
 #'
 #' @return A numeric vector of p-values, length is the same as the number of iterations
 #' @export
@@ -16,23 +18,36 @@
 #' #200 iterations run on 2 cores
 #' PValsParallel(df = polity, iterations = 200, cores = 2)
 #'
-PValsParallel <- function(df, iterations, cores = 1, stationary = FALSE){
+PValsParallel <- function(df, iterations, cores = 1, difference = TRUE, threshold = 0.1){
   require(foreach)
   myCluster <- parallel::makeCluster(cores, # number of cores to use
                                      type = "PSOCK") # type of cluster
   doParallel::registerDoParallel(myCluster)
   results <- foreach(n = 1:iterations, .combine = "rbind", .packages = "plm") %dopar% {
-    x = try(plm::purtest(df), silent = T)
-    if(class(x) == "try-error" | stationary == FALSE){
-      ## do this if non stationary
-      binindex <- sample(c(1, 2), size = ncol(df), replace = T)
-      bin1 <- df[, binindex == 1]
-      bin2 <- df[, binindex == 2]
-      bindat <- cbind(apply(bin1, 1, mean), apply(bin2, 1, mean))
-      bindat <- diff(bindat, 1)
-      t <- plm::purtest(bindat)
-      pvals <-  t$statistic$p.value
-      return(pvals)
+    if(difference == TRUE){
+      node_sd <- apply(df, 2, sd)
+      x <- plm::purtest(df[, node_sd > 0], test = "hadri", exo = "intercept")
+      if(x$statistic$p.value < threshold){
+        ## do this if non stationary
+        binindex <- sample(c(1, 2), size = ncol(df), replace = T)
+        bin1 <- df[, binindex == 1]
+        bin2 <- df[, binindex == 2]
+        bindat <- cbind(apply(bin1, 1, mean), apply(bin2, 1, mean))
+        bindat <- diff(bindat, 1)
+        t <- plm::purtest(bindat)
+        pvals <-  t$statistic$p.value
+        return(pvals)
+      }
+      else{
+        ## do this if stationary
+        binindex <- sample(c(1, 2), size = ncol(df), replace = T)
+        bin1 <- df[, binindex == 1]
+        bin2 <- df[, binindex == 2]
+        bindat <- cbind(apply(bin1, 1, mean), apply(bin2, 1, mean))
+        t <- plm::purtest(bindat)
+        pvals <-  t$statistic$p.value
+        return(pvals)
+      }
     } else {
       ## do this if stationary
       binindex <- sample(c(1, 2), size = ncol(df), replace = T)
