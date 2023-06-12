d838a385f1437c0056cbede04688b73cf884d724
rjf9 <rjf5775@psu.edu>
2023-06-12 09:43:51 -0400

    Update unit tests

--- tests/testthat/test-contagion.R
+++ tests/testthat/test-contagion.R
@@ -4,12 +4,27 @@ test_that("lag window works", {
 })
 
 
-test_that("differencing works", {
+test_that("stationary data", {
   set.seed(3933)
   x1 <- rbinom(n = 100, size = 1, prob = 0.5)
   x2 <- rbinom(n = 100, size = 1, prob = 0.5)
   dfS <- data.frame(x1, x2, x1, x2, x1, x2, x1, x2, x1)
-  expect_equal(round(no_diff_data(dfS, 1), 5), list(1.00000, "results" = c(0.45719, 0.00543, 0.05901)))
+  expect_equal(round(unname(no_diff_data(dfS, 1)[-1]), 5), c(0.47815, -0.00758, 0.04599))
+})
+
+test_that("non stationary data", {
+  set.seed(3933)
+  x1 <- runif(100, 0, 1) * seq(1:100)
+  x2 <- runif(100, 0, 1) * seq(1:100)
+  x3 <- runif(100, 0, 1) * seq(1:100)
+  x4 <- runif(100, 0, 1) * seq(1:100)
+  x5 <- runif(100, 0, 1) * seq(1:100)
+  x6 <- runif(100, 0, 1) * seq(1:100)
+  x7 <- runif(100, 0, 1) * seq(1:100)
+  x8 <- runif(100, 0, 1) * seq(1:100)
+  dfS <- data.frame(x1, x2, x3, x4, x5, x6, x7, x8)
+  lag_pc_test(dfS, 10, 1, T)
+  expect_equal(round(unname(diff_data(dfS, 1)[-1]), 5), c(0.47815, -0.00758, 0.04599))
 })
 
 test_that("right test", {
@@ -17,5 +32,9 @@ test_that("right test", {
   x1 <- rbinom(n = 100, size = 1, prob = 0.5)
   x2 <- rbinom(n = 100, size = 1, prob = 0.5)
   dfS <- data.frame(x1, x2, x1, x2, x1, x2, x1, x2, x1)
-  expect_equal(lag_pc_test(dfS, 1, 1, F), no_diff_data(dfS, 1))
+  #set.seed(3933)
+  nodif <- round(no_diff_data(dfS, 1)[-1], 3)
+  #nodif
+  set.seed(3933)
+  expect_equal(round(lag_pc_test(dfS, 1, 1, F, 0.1, 1, F), 3), nodif)
 })
