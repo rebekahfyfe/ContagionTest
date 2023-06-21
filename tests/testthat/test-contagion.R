99842ed77a06a8405d4c7043517fd4529a94d7cb
rjf9 <rjf5775@psu.edu>
2023-06-20 20:30:26 -0400

    Update unit test for running correct function

--- tests/testthat/test-contagion.R
+++ tests/testthat/test-contagion.R
@@ -5,15 +5,15 @@ test_that("lag window works", {
 
 
 test_that("stationary data", {
-  set.seed(3933)
+  set.seed(39)
   x1 <- rbinom(n = 100, size = 1, prob = 0.5)
   x2 <- rbinom(n = 100, size = 1, prob = 0.5)
   dfS <- data.frame(x1, x2, x1, x2, x1, x2, x1, x2, x1)
-  expect_equal(round(unname(no_diff_data(dfS, 1)[-1]), 5), c(0.47815, -0.00758, 0.04599))
+  expect_equal(round(unname(no_diff_data(dfS, 1)[-1]), 2), c(0.46, 0.01, 0.01))
 })
 
 test_that("non stationary data", {
-  set.seed(3933)
+  set.seed(39)
   x1 <- runif(100, 0, 1) * seq(1:100)
   x2 <- runif(100, 0, 1) * seq(1:100)
   x3 <- runif(100, 0, 1) * seq(1:100)
@@ -23,18 +23,37 @@ test_that("non stationary data", {
   x7 <- runif(100, 0, 1) * seq(1:100)
   x8 <- runif(100, 0, 1) * seq(1:100)
   dfS <- data.frame(x1, x2, x3, x4, x5, x6, x7, x8)
-  lag_pc_test(dfS, 10, 1, T)
-  expect_equal(round(unname(diff_data(dfS, 1)[-1]), 5), c(0.47815, -0.00758, 0.04599))
+  expect_equal(round(unname(diff_data(dfS, 1)[-1]), 2), c(0.93, -0.40, 0.04))
 })
 
-test_that("right test", {
-  set.seed(3933)
+test_that("right test stationary", {
+  set.seed(39)
   x1 <- rbinom(n = 100, size = 1, prob = 0.5)
   x2 <- rbinom(n = 100, size = 1, prob = 0.5)
-  dfS <- data.frame(x1, x2, x1, x2, x1, x2, x1, x2, x1)
-  #set.seed(3933)
-  nodif <- round(no_diff_data(dfS, 1)[-1], 3)
-  #nodif
-  set.seed(3933)
-  expect_equal(round(lag_pc_test(dfS, 1, 1, F, 0.1, 1, F), 3), nodif)
+  x3 <- rbinom(n = 100, size = 1, prob = 0.5)
+  dfS <- data.frame(x1, x2, x3, x1, x2, x3, x1, x2, x3, x1, x2, x3)
+  set.seed(39)
+  nodif <- round(no_diff_data(dfS, 1)[-1], 1)
+  set.seed(39)
+  main <- round(lag_pc_test(dfS, 1, 1, F, 0.1, 1, F), 1)
+  expect_equal(main, nodif)
 })
+
+test_that("right test non stationary", {
+  set.seed(39)
+  x1 <- runif(100, 0, 1) * seq(1:100)
+  x2 <- runif(100, 0, 1) * seq(1:100)
+  x3 <- runif(100, 0, 1) * seq(1:100)
+  x4 <- runif(100, 0, 1) * seq(1:100)
+  x5 <- runif(100, 0, 1) * seq(1:100)
+  x6 <- runif(100, 0, 1) * seq(1:100)
+  x7 <- runif(100, 0, 1) * seq(1:100)
+  x8 <- runif(100, 0, 1) * seq(1:100)
+  dfS <- data.frame(x1, x2, x3, x4, x5, x6, x7, x8)
+  set.seed(39)
+  main <- round(lag_pc_test(dfS, 1, 1, T, 0.1, 1, F), 2)
+  set.seed(39)
+  diff <- round(diff_data(dfS, 1)[-1], 2)
+  expect_equal(main, diff)
+})
+
