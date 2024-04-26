7d1170d562855c239e60353172d3fe3fd0e519ae
rjf9 <rjf5775@psu.edu>
2024-04-26 10:54:06 -0400

    Added color parameter to plotting function

--- R/plotting.R
+++ R/plotting.R
@@ -8,13 +8,16 @@
 #' @param pval.xcoord The x-coordinate for where the p-value should be displayed on the graph. Default is 0.
 #' @param pval.ycoord The y-coordinate for where the p-value should be displayed on the graph. Default is 0.
 #' @param title The title to be displayed on the plot. Default is an empty string.
+#' @param fillCol The fill color for the graph. Default is "lightsteelblue"
+#' @param colorCol The outline color for the graph. Default is "lightsteelblue4"
 #'
 #' @return
 #' @export
 #'
 #' @examples
 density_graph <- function(model, n, mean.xcoord = 0, mean.ycoord = 1,
-                          pval.xcoord = 0, pval.ycoord = 0, title = ""){
+                          pval.xcoord = 0, pval.ycoord = 0, title = "",
+                          fillCol = "lightsteelblue", colorCol = "lightsteelblue4"){
   require(ggplot2)
   model <- as.data.frame(model)
   # Mean of the results from the n iterations of the SH test
@@ -28,8 +31,8 @@ density_graph <- function(model, n, mean.xcoord = 0, mean.ycoord = 1,
     ylab("Density") +
     ggtitle(title) +
     theme_light() +
-    geom_density(fill = "lightgrey", color = "black") +
-    geom_vline(aes(xintercept = mean), colour = "black") +
+    geom_density(fill = fillCol, color = colorCol) +
+    geom_vline(aes(xintercept = mean), colour = colorCol) +
     annotate("text", x = (mean.xcoord), y = mean.ycoord,
              label = paste("Mean:", round(mean, 4)), color = "gray20") +
     geom_vline(aes(xintercept = 0),color = "gray20", linetype = "dashed") +
